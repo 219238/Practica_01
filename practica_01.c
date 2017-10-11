9350402ec82cb03b42ea742097464e4e46b6a46e
219238 <32716313+219238@users.noreply.github.com>
2017-10-11 13:45:55 -0500

    Update practica_01.c

--- practica_01.c
+++ practica_01.c
@@ -1,1 +1,27 @@
-copiar y pegar código
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+	//método de la burbuja
+	int vect[25], aux = 0, cont = 0;
+	srand(time(NULL));
+	for (int z = 0; z < 25; z ++){
+		vect[z]= rand();
+	}
+	
+	for (int i = 0;  i < 25; i ++){
+		for (int j = 0; j < (25 - i); j ++){
+			cont ++;
+			if (vect[j]>vect[j + 1]){
+				aux = vect[j];
+				vect[j] = vect[j + 1];
+				vect[j + 1] = aux;
+				cont ++;
+			}
+		}
+	}
+	for (int x = 0; x < 25; x ++){
+		printf("[%i]", vect[x]);
+	}
+}
